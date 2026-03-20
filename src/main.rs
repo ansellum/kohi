2065d026075fbbc55b41971dec30515a4273aaaa
ansellum <jacobvogt27@gmail.com>
2026-03-19 18:44:54 -0700

    Added primitive command handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
 use std::env;
+use std::process;
 
-pub mod bean;
+//pub mod bean;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Root command: {}", config.root);
+    println!("Subcommand: {}", config.command);
+}
+
+struct Config {
+    root: String,
+    command: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let root = args[1].clone();
+        let command = args[2].clone();
+
+        Ok(Config { root, command })
+    }
 }
