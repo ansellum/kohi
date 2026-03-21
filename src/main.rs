bddfe042b37483003725eb2be7c0a1eb5eb237aa
marche <jacobvogt27@gmail.com>
2026-03-20 17:36:43 -0700

    Created command skeletons

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,86 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 //pub mod bean;
 
 #[derive(Parser)]
+#[command(version, about)]
+#[command(next_line_help = true)]
 struct Cli {
-    root: String,
-    command: String,
+    #[command(subcommand)]
+    command: Modes,
+}
+
+#[derive(Subcommand)]
+enum Modes {
+    Equipment(EquipmentArgs),
+    Bag(BagArgs),
+    Coffee(CoffeeArgs),
+    Brew(BrewArgs),
+}
+
+#[derive(Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct EquipmentArgs {
+    #[command(subcommand)]
+    command: EquipmentCommands,
+}
+
+#[derive(Subcommand)]
+enum EquipmentCommands {
+    Add,
+    Remove,
+    List,
+}
+
+#[derive(Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct BagArgs {
+    #[command(subcommand)]
+    command: BagCommands,
+}
+
+#[derive(Subcommand)]
+enum BagCommands {
+    Add,
+    List,
+}
+
+#[derive(Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct CoffeeArgs {
+    #[command(subcommand)]
+    command: CoffeeCommands,
+}
+
+#[derive(Subcommand)]
+enum CoffeeCommands {
+    Add,
+    Remove,
+    List,
+}
+
+#[derive(Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct BrewArgs {
+    #[command(subcommand)]
+    command: BrewCommands,
+}
+
+#[derive(Subcommand)]
+enum BrewCommands {
+    Add,
+    Remove,
+    List,
 }
 
 fn main() {
     let args = Cli::parse();
 
-    println!("Root:     {}", args.root);
-    println!("Command:  {}", args.command);
+    // match &args.command {
+    //    TODO
+    // }
 }
