43dbd29169c131ba27f1c6d764906ab62bba9d6c
ansellum <marche@Shibuya.(none)>
2026-03-27 22:03:27 -0700

    Added serde support

--- src/equipment.rs
+++ src/equipment.rs
@@ -1,17 +1,26 @@
 use jiff::Timestamp;
+use serde::{Serialize, Deserialize};
+use std::io::{self, Write};
+//use serde_json::Result;
 
+#[derive(Serialize, Deserialize)]
 enum EquipmentType {
     Brewer,
     Grinder,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Equipment {
     id: u32,
     name: String,
 
-    machine: EquipmentType,
+    equipment_type: EquipmentType,
     purchase_date: Timestamp,
     decommission_date: Timestamp,
     price: u32,
     timestamp: Timestamp,
 }
+
+//pub fn equipment_from_json() -> Equipment {
+    
+//}
