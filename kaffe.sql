e0ed499fead8ed8e454c7f82b2dc136288ea59f3
ansellum <marche@Shibuya.(none)>
2026-03-28 19:34:10 -0700

    Updated schema

--- kaffe.sql
+++ kaffe.sql
@@ -1,49 +1,55 @@
--- CREATE TABLE equipment (
---     id INTEGER PRIMARY KEY,
---     name TEXT NOT NULL,
---     type TEXT NOT NULL,
---     purchase_date DATETIME NOT NULL,
---     price_ct INTEGER NOT NULL,
---     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE equipment (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    purchase_date DATETIME NOT NULL,
+    decommission_date DATETIME,
+    price_ct INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
--- CREATE TABLE coffee (
---     id INTEGER PRIMARY KEY,
---     roaster TEXT NOT NULL,
---     name TEXT NOT NULL,
---     type TEXT NOT NULL,
---     country TEXT NOT NULL,
---     region TEXT,
---     altitutde_lower_m INTEGER,
---     altitutde_upper_m INTEGER,
---     process TEXT NOT NULL,
---     tasting_notes TEXT NOT NULL,
---     decaf INTEGER NOT NULL,
---     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE coffee (
+    id INTEGER PRIMARY KEY,
+    roaster TEXT NOT NULL,
+    name TEXT NOT NULL,
+    roast_level TEXT NOT NULL,
+    type TEXT NOT NULL,
+    country TEXT,
+    region TEXT,
+    farm TEXT,
+    producer TEXT,
+    varietals TEXT,
+    altittude_masl INTEGER,
+    altitutde_lower_m INTEGER,
+    altitutde_upper_m INTEGER,
+    process TEXT NOT NULL,
+    tasting_notes TEXT NOT NULL,
+    decaf INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
--- CREATE TABLE bag (
---     id INTEGER PRIMARY KEY,
---     coffee_id INTEGER REFERENCES coffee(id), --foreign key
---     weight_g INTEGER NOT NULL,
---     roast_date DATETIME NOT NULL,
---     open_date DATETIME,
---     empty_date DATETIME,
---     purchase_date DATETIME NOT NULL,
---     price_ct INTEGER NOT NULL,
---     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE bag (
+    id INTEGER PRIMARY KEY,
+    coffee_id INTEGER REFERENCES coffee(id), --foreign key
+    roast_date DATETIME NOT NULL,
+    open_date DATETIME,
+    empty_date DATETIME,
+    purchase_date DATETIME NOT NULL,
+    weight_g INTEGER NOT NULL,
+    price_ct INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
--- CREATE TABLE brew (
---     id INTEGER PRIMARY KEY,
---     bag_id INTEGER REFERENCES bag(id), --foreign key
---     grinder_id INTEGER REFERENCES equipment(id), --foreign key
---     brewer_id INTEGER REFERENCES equipment(id), --foreign key
---     grind_level INTEGER NOT NULL,
---     coffee_g INTEGER NOT NULL,
---     water_g INTEGER,
---     brew_g INTEGER NOT NULL,
---     rating INTEGER NOT NULL,
---     notes TEXT,
---     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE brew (
+    id INTEGER PRIMARY KEY,
+    bag_id INTEGER REFERENCES bag(id), --foreign key
+    grinder_id INTEGER REFERENCES equipment(id), --foreign key
+    brewer_id INTEGER REFERENCES equipment(id), --foreign key
+    grind_level INTEGER NOT NULL,
+    coffee_g INTEGER NOT NULL,
+    water_g INTEGER,
+    brew_g INTEGER NOT NULL,
+    rating INTEGER NOT NULL,
+    notes TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
